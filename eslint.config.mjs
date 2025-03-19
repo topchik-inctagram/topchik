326b8d66a589cccde239ac021f6a28f7b08c03b4
Evgeniy Ivanov <p8635241@gmail.com>
2025-03-20 01:31:51 +0500

    feat add vitest test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,8 +10,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  "@it-incubator/eslint-config",
+  ...compat.extends('next/core-web-vitals', 'next/typescript', '@it-incubator/eslint-config'),
+  {
+    files: ['**/*.test.{js,ts,jsx,tsx}', '**/__tests__/**/*.{js,ts,jsx,tsx}'],
+    extends: ['plugin:vitest/recommended'], // Подключаем только Vitest
+    env: {
+      'vitest-globals/env': true, // Поддержка глобальных переменных Vitest
+    },
+    rules: {
+      'vitest/no-disabled-tests': 'warn', // Предупреждать о test.skip
+      'vitest/no-focused-tests': 'error', // Запрещать test.only
+      'vitest/no-identical-title': 'error', // Не допускать одинаковые названия тестов
+      'vitest/prefer-to-be': 'warn', // Предупреждать о использовании `expect(x).toBe(true)`
+    },
+  },
 ];
 
 export default eslintConfig;
