4b99639d01099ddf010730fdb6f03dfd34dbb215
Evgeniy Ivanov <p8635241@gmail.com>
2025-03-20 02:14:12 +0500

    fix testing files

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['.storybook/vitest.setup.ts'],
+    include: ['tests/**/*.test.ts', 'tests/**/*.test.tsx'], // Добавляем путь к тестам
+  },
+});
