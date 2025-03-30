6db4e5113d8995f7c7b789a2ad3c79b274af98cb
Evgeniy Ivanov <p8635241@gmail.com>
2025-03-31 03:05:06 +0500

    IN-19 feat: add style classes for scrollarea

--- shared/components/ScrollArea/ScrollArea.tsx
+++ shared/components/ScrollArea/ScrollArea.tsx
@@ -1,3 +1,35 @@
-export const ScrollArea = () => {
-  return <div>hYE</div>
+import s from './ScrollArea.module.scss'
+import {
+  ScrollAreaThumb,
+  ScrollAreaScrollbar,
+  ScrollAreaViewport,
+  Root,
+  ScrollAreaCorner,
+  type ScrollAreaProps,
+} from '@radix-ui/react-scroll-area'
+import { clsx } from 'clsx'
+
+export const ScrollArea = (props: ScrollAreaProps) => {
+  const { children, className, ...rest } = props
+
+  const classNames = {
+    root: clsx(s.root, className),
+    viewPort: s.viewPort,
+    scrollBar: s.scrollBar,
+    areaThumb: s.areaThumb,
+    corner: s.corner,
+  }
+
+  return (
+    <Root className={classNames.root} {...rest}>
+      <ScrollAreaViewport className={classNames.viewPort}>{children}</ScrollAreaViewport>
+      <ScrollAreaScrollbar className={classNames.scrollBar} orientation="vertical">
+        <ScrollAreaThumb className={classNames.areaThumb} />
+      </ScrollAreaScrollbar>
+      <ScrollAreaScrollbar className={classNames.scrollBar} orientation="horizontal">
+        <ScrollAreaThumb className={classNames.areaThumb} />
+      </ScrollAreaScrollbar>
+      <ScrollAreaCorner className={classNames.corner} />
+    </Root>
+  )
 }
