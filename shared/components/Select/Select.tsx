17ad72cf92ed2ed42a04f4d33d9c3470d7ee8a5e
Vasyaruberoid <84591840+Vasyaruberoid@users.noreply.github.com>
2025-03-30 22:26:31 +0300

    refactor Select IN-7

--- shared/components/Select/Select.tsx
+++ shared/components/Select/Select.tsx
@@ -1,82 +1,113 @@
-import { Select as RadixSelect } from 'radix-ui'
-import styles from './Select.module.scss'
-import classnames from 'classnames'
+import * as SelectRadix from '@radix-ui/react-select'
+import { clsx } from 'clsx'
 import ArrowDown from '../../../public/icons/ArrowIosDownOutline'
 import ArrowUp from '../../../public/icons/ArrowIosUp'
-import { Typography } from '../Typography'
-import { forwardRef, ReactNode, useState } from 'react'
+import { ComponentPropsWithoutRef, ReactNode, useState } from 'react'
+import s from './Select.module.scss'
+import { Label } from '../Label/Label'
 
-export type SelectProps = {
-  onChange?: (value: string) => void
-  className?: string
-  disabled?: boolean
+export type SelectOption = {
   value: string
   label: string
-  options: { value: string; label: string }[]
+  icon?: ReactNode
 }
 
-type SelectItemProps = {
+export type SelectProps = {
+  className?: string
+  options: SelectOption[]
+  placeholder?: string
+  disabled?: boolean
+  label?: string
+  errorMessage?: string
+  value?: string
+  onChangeSelect?: (value: string) => void
+  defaultValue?: string
+  ref?: React.Ref<HTMLButtonElement>
+  pagination?: boolean
+} & ComponentPropsWithoutRef<typeof SelectRadix.Root>
+
+export const Select = (props: SelectProps) => {
+  const {
+    className,
+    options,
+    placeholder = 'Select-Box',
+    disabled,
+    label,
+    errorMessage,
+    value,
+    onChangeSelect,
+    ref,
+    defaultValue = '',
+    pagination = false,
+    ...rest
+  } = props
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleValueChange = (val: string) => {
+    if (pagination) {
+      const numValue = parseInt(val)
+      onChangeSelect?.(isNaN(numValue) ? val : numValue.toString())
+    } else {
+      onChangeSelect?.(val)
+    }
+  }
+
+  return (
+    <div className={clsx(s.selectWrapper, className, { [s.pagination]: pagination })}>
+      {label && <Label>{label}</Label>}
+      <SelectRadix.Root
+        value={value}
+        onValueChange={handleValueChange}
+        disabled={disabled}
+        defaultValue={defaultValue}
+        onOpenChange={setIsOpen}
+        {...rest}
+      >
+        <SelectRadix.Trigger ref={ref} className={clsx(s.trigger, errorMessage && s.error)}>
+          <div className={s.valueContainer}>
+            {value && options.find(opt => opt.value === value)?.icon}
+            <SelectRadix.Value placeholder={placeholder}>
+              {value && options.find(opt => opt.value === value)?.label}
+            </SelectRadix.Value>
+          </div>
+          <SelectRadix.Icon className={s.icon}>
+            {isOpen ? <ArrowUp /> : <ArrowDown />}
+          </SelectRadix.Icon>
+        </SelectRadix.Trigger>
+
+        <SelectRadix.Portal>
+          <SelectRadix.Content className={s.content} position="popper" sideOffset={5}>
+            <SelectRadix.Viewport className={s.viewport}>
+              {options.map(option => (
+                <SelectItem key={option.value} value={option.value}>
+                  <div className={s.optionContent}>
+                    {option.icon && <span className={s.optionIcon}>{option.icon}</span>}
+                    {option.label}
+                  </div>
+                </SelectItem>
+              ))}
+            </SelectRadix.Viewport>
+          </SelectRadix.Content>
+        </SelectRadix.Portal>
+      </SelectRadix.Root>
+      {errorMessage && <span className={s.errorMessage}>{errorMessage}</span>}
+    </div>
+  )
+}
+
+const SelectItem = ({
+  children,
+  className,
+  ...props
+}: {
   children: ReactNode
   className?: string
   value: string
-}
-
-export const Select = ({ label, className, disabled, value, options, onChange }: SelectProps) => {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const handleOpenChange = (open: boolean) => {
-    setIsOpen(open)
-  }
-
-  const handleSelectChange = (newValue: string) => {
-    if (onChange) {
-      onChange(newValue)
-    }
-  }
-
-  const SelectItem = forwardRef<HTMLDivElement, SelectItemProps>(
-    ({ children, className, ...props }, forwardedRef) => {
-      return (
-        <RadixSelect.Item
-          className={classnames(styles.Item, className)}
-          {...props}
-          ref={forwardedRef}
-        >
-          <RadixSelect.ItemText>{children}</RadixSelect.ItemText>
-          <RadixSelect.ItemIndicator className={styles.ItemIndicator}></RadixSelect.ItemIndicator>
-        </RadixSelect.Item>
-      )
-    }
-  )
-
+}) => {
   return (
-    <RadixSelect.Root
-      value={value}
-      onOpenChange={handleOpenChange}
-      onValueChange={handleSelectChange}
-    >
-      <Typography as="label" variant="medium_14">
-        {label}
-      </Typography>
-      <RadixSelect.Trigger className={styles.Trigger} aria-label="Food">
-        <RadixSelect.Value placeholder="Select-Box" />
-        <RadixSelect.Icon className={styles.Icon}>
-          {isOpen ? <ArrowUp /> : <ArrowDown />}
-        </RadixSelect.Icon>
-      </RadixSelect.Trigger>
-      <RadixSelect.Portal>
-        <RadixSelect.Content side="bottom" align="start" className={styles.Content}>
-          <RadixSelect.Viewport className={styles.Viewport}>
-            <RadixSelect.Group>
-              {options.map(option => (
-                <SelectItem key={option.value} value={option.value}>
-                  {option.label}
-                </SelectItem>
-              ))}
-            </RadixSelect.Group>
-          </RadixSelect.Viewport>
-        </RadixSelect.Content>
-      </RadixSelect.Portal>
-    </RadixSelect.Root>
+    <SelectRadix.Item className={clsx(s.item, className)} {...props}>
+      <SelectRadix.ItemText>{children}</SelectRadix.ItemText>
+    </SelectRadix.Item>
   )
 }
