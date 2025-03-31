0ba53a13d67384f28cc89115cbd3fb6d994e8df1
Evgeniy Ivanov <p8635241@gmail.com>
2025-04-01 03:44:54 +0500

    IN-20 feat: navbar component ready for review, desktop and mobile version

--- shared/components/Navbar/Navbar.tsx
+++ shared/components/Navbar/Navbar.tsx
@@ -20,103 +20,100 @@ import {
   TrendUpOutline,
 } from '@/public'
 import { Typography } from '@/shared/components'
+import type { ComponentPropsWithRef } from 'react'
 
 type Props = {
-  className?: string
   isMobile?: boolean
-}
+} & ComponentPropsWithRef<'nav'>
 
 export const Navbar = (props: Props) => {
-  const { className, isMobile, ...rest } = props
+  const { isMobile, ...rest } = props
 
-  // if you want to disable link you need to add data-disabled='disabled' in link props
-  return isMobile ? (
-    <MobileNavbar className={className} {...rest} />
-  ) : (
-    <DesktopNavbar className={className} {...rest} />
-  )
+  return isMobile ? <MobileNavbar {...rest} /> : <DesktopNavbar {...rest} />
 }
 
-function MobileNavbar(className: any) {
+function MobileNavbar({ className, ...rest }: ComponentPropsWithRef<'nav'>) {
   const classNames = {
     nav: clsx(s.mobileNav, className),
+    mobileContainer: s.mobileContainer,
   }
   const active = true
   return (
-    <nav className={classNames.nav}>
-      <ul>
+    <nav className={classNames.nav} {...rest}>
+      <ul className={classNames.mobileContainer}>
         <li>
-          <Link href="#">{active ? <HomeOutline /> : <Home />}</Link>
+          <Link href="#">{active ? <Home /> : <HomeOutline />}</Link>
         </li>
         <li>
-          <Link href="#">{active ? <PlusSquareOutline /> : <PlusSquare />}</Link>
+          <Link href="#">{active ? <PlusSquare /> : <PlusSquareOutline />}</Link>
         </li>
         <li>
-          <Link href="#">{active ? <MessageCircleOutline /> : <MessageCircle />}</Link>
+          <Link href="#">{active ? <MessageCircle /> : <MessageCircleOutline />}</Link>
         </li>
         <li>
-          <Link href="#">{active ? <SearchOutline /> : <Search />}</Link>
+          <Link href="#">{active ? <Search /> : <SearchOutline />}</Link>
         </li>
         <li>
-          <Link href="#">{active ? <PersonOutline /> : <Person />}</Link>
+          <Link href="#">{active ? <Person /> : <PersonOutline />}</Link>
         </li>
       </ul>
     </nav>
   )
 }
 
-function DesktopNavbar(className: any) {
+function DesktopNavbar({ className, ...rest }: ComponentPropsWithRef<'nav'>) {
   const classNames = {
     nav: clsx(s.desktopNav, className),
     firstContainer: clsx(s.desktopContainer, s.desktopFirstContainer),
     secondContainer: clsx(s.desktopSecondContainer, s.desktopContainer),
   }
   const active = true
+  // if you want to disable link you need to add data-disabled='disabled' in link props
   return (
-    <nav className={classNames.nav}>
+    <nav className={classNames.nav} {...rest}>
       <ul>
         <div className={classNames.firstContainer}>
           <li>
-            <Typography as={Link} data-disabled="disabled" href="#">
-              {active ? <HomeOutline /> : <Home />} Feed
+            <Typography as={Link} data-disabled="disabled" href="#" variant="medium_14">
+              {active ? <Home /> : <HomeOutline />} Feed
             </Typography>
           </li>
           <li>
             <Typography as={Link} href="#" variant="medium_14">
-              {active ? <PlusSquareOutline /> : <PlusSquare />} Create
+              {active ? <PlusSquare /> : <PlusSquareOutline />} Create
             </Typography>
           </li>
           <li>
             <Typography as={Link} href="#" variant="medium_14">
-              {active ? <PersonOutline /> : <Person />} My Profile
+              {active ? <Person /> : <PersonOutline />} My Profile
             </Typography>
           </li>
           <li>
             <Typography as={Link} href="#" variant="medium_14">
-              {active ? <MessageCircleOutline /> : <MessageCircle />} Messenger
+              {active ? <MessageCircle /> : <MessageCircleOutline />} Messenger
             </Typography>
           </li>
           <li>
             <Typography as={Link} href="#" variant="medium_14">
-              {active ? <SearchOutline /> : <Search />} Search
+              {active ? <Search /> : <SearchOutline />} Search
             </Typography>
           </li>
         </div>
         <div className={classNames.secondContainer}>
           <li>
             <Typography as={Link} href="#" variant="medium_14">
-              {active ? <TrendUpOutline /> : <TrendUp />} Statistics
+              {active ? <TrendUp /> : <TrendUpOutline />} Statistics
             </Typography>
           </li>
           <li>
             <Typography as={Link} href="#" variant="medium_14">
-              {active ? <BookmarkOutline /> : <Bookmark />} Favorites
+              {active ? <Bookmark /> : <BookmarkOutline />} Favorites
             </Typography>
           </li>
         </div>
         <li>
           <Typography as={Link} href="#" variant="medium_14">
-            {active ? <LogOutOutline /> : <LogOut />} Log Out
+            {active ? <LogOut /> : <LogOutOutline />} Log Out
           </Typography>
         </li>
       </ul>
