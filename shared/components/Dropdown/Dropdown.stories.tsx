d8c23bbe7ad6b64776a120a51cfbcd80db23c9ac
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2025-04-01 22:01:10 +0300

    IN-15 feat: add stories for Dropdown

--- shared/components/Dropdown/Dropdown.stories.tsx
+++ shared/components/Dropdown/Dropdown.stories.tsx
@@ -1,5 +1,12 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Dropdown } from './'
+import { Dropdown, DropdownArrow, DropdownItem, DropdownLabel, DropdownSeparator } from './'
+import { NotificationItem } from './dropDownItems/notificationItem/NotificationItem'
+import { NotificationBell } from './dropDownItems/notificationItem/notificationBell/NotificationBell'
+import {
+  ActionDropdownDots,
+  ActionDropdownItem,
+} from './dropDownItems/actionDropdownItem/ActionDropdownItem'
+import { Edit2Outline, TrashOutline } from '@/public'
 
 const meta = {
   component: Dropdown,
@@ -10,15 +17,102 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const DropdownMenu: Story = {
+export const DropdownMenuForActions: Story = {
   args: {
-    trigger: <button>+</button>,
-    children: 'Hello',
+    trigger: (
+      <button>
+        <ActionDropdownDots />
+      </button>
+    ),
   },
   render: args => {
     return (
-      <div>
-        <Dropdown {...args} />
+      <div style={{ marginLeft: '150px' }}>
+        <Dropdown {...args}>
+          <DropdownItem>
+            <ActionDropdownItem icon={<Edit2Outline />} label="Edit Post" />
+          </DropdownItem>
+          <DropdownItem>
+            <ActionDropdownItem icon={<TrashOutline />} label="Delete Post" />
+          </DropdownItem>
+        </Dropdown>
+      </div>
+    )
+  },
+}
+
+export const DropdownMenuForNotifications: Story = {
+  args: {
+    trigger: (
+      <button>
+        <NotificationBell count={3} />
+      </button>
+    ),
+  },
+  render: args => {
+    return (
+      <div style={{ marginLeft: '300px' }}>
+        <Dropdown {...args}>
+          <div style={{ maxHeight: '410px', overflowY: 'auto' }}>
+            <DropdownLabel>Уведомления</DropdownLabel>
+            <DropdownSeparator />
+            <DropdownItem>
+              <NotificationItem
+                title="Новое уведомление!"
+                tag="Новое"
+                message="Следующий платеж у вас спишется через 1 день"
+                date="1 день назад"
+              />
+            </DropdownItem>
+
+            <DropdownSeparator />
+            <DropdownItem>
+              <NotificationItem
+                title="Новое уведомление!"
+                tag="Новое"
+                message="Ваша подписка истекает через 7 дней"
+                date="1 день назад"
+              />
+            </DropdownItem>
+            <DropdownSeparator />
+            <DropdownItem>
+              <NotificationItem
+                title="Новое уведомление!"
+                tag="Новое"
+                message="Ваша подписка истекает через 7 дней"
+                date="1 день назад"
+              />
+            </DropdownItem>
+            <DropdownSeparator />
+            <DropdownItem>
+              <NotificationItem
+                title="Новое уведомление!"
+                tag="Новое"
+                message="Ваша подписка истекает через 7 дней"
+                date="1 день назад"
+              />
+            </DropdownItem>
+            <DropdownSeparator />
+            <DropdownItem>
+              <NotificationItem
+                title="Новое уведомление!"
+                tag="Новое"
+                message="Ваша подписка истекает через 7 дней"
+                date="1 день назад"
+              />
+            </DropdownItem>
+            <DropdownSeparator />
+            <DropdownItem>
+              <NotificationItem
+                title="Новое уведомление!"
+                tag="Новое"
+                message="Ваша подписка истекает через 7 дней"
+                date="1 день назад"
+              />
+            </DropdownItem>
+          </div>
+          <DropdownArrow />
+        </Dropdown>
       </div>
     )
   },
