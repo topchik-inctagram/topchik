972a2cfa3cd4f0f5367f6748c60a8e0a8ce598e5
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2025-04-01 21:59:33 +0300

    IN-15 feat: add components for NotificationItem

---
+++ shared/components/Dropdown/dropDownItems/notificationItem/NotificationItem.tsx
@@ -0,0 +1,51 @@
+import { ComponentPropsWithoutRef } from 'react'
+import s from './NotificationItem.module.scss'
+import { Typography } from '@/shared/components'
+
+type NotificationItemProps = {
+  title: string
+  tag?: string
+  message: string
+  date: string
+} & ComponentPropsWithoutRef<'div'> &
+  Omit<ComponentPropsWithoutRef<'div'>, 'onChange'>
+
+export const NotificationItem = ({
+  title,
+  tag,
+  message,
+  date,
+  onClick,
+  ...rest
+}: NotificationItemProps) => {
+  const classNames = {
+    notification: s.notification,
+    header: s.header,
+    title: s.title,
+    tag: s.tag,
+    message: s.message,
+    date: s.date,
+  }
+
+  return (
+    <div className={classNames.notification} onClick={onClick} {...rest}>
+      <div className={classNames.header}>
+        <Typography as="h4" variant="bold_14" className={classNames.title}>
+          {title}
+        </Typography>
+        {tag && (
+          <Typography as="span" variant="regular_14" className={classNames.tag}>
+            {tag}
+          </Typography>
+        )}
+      </div>
+      <Typography as="p" variant="regular_14" className={classNames.message}>
+        {message}
+      </Typography>
+
+      <Typography as="span" variant="small" className={classNames.date}>
+        {date}
+      </Typography>
+    </div>
+  )
+}
