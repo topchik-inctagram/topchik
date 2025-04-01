9dbd5fe79ea597f71f0827510235f938cc7ded27
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2025-04-01 17:15:06 +0300

    IN-15 feat: add DropdownLabel, DropdownSeparator, DropdownArrow, DropdownItem

--- shared/components/Dropdown/Dropdown.tsx
+++ shared/components/Dropdown/Dropdown.tsx
@@ -13,7 +13,7 @@ export const Dropdown = forwardRef<React.ComponentRef<typeof DropdownMenu.Conten
   ({ trigger, align = 'end', children, className, ...rest }, ref) => {
     const classNames = {
       content: clsx(s.content, className),
-      trigger: s.trigger,
+      trigger: clsx(s.trigger, className),
     }
 
     return (
@@ -23,7 +23,7 @@ export const Dropdown = forwardRef<React.ComponentRef<typeof DropdownMenu.Conten
         </DropdownMenu.Trigger>
 
         <DropdownMenu.Portal>
-          <DropdownMenu.Content className={classNames.content} align={align} ref={ref}>
+          <DropdownMenu.Content className={classNames.content} align={align} ref={ref} {...rest}>
             {children}
           </DropdownMenu.Content>
         </DropdownMenu.Portal>
@@ -32,3 +32,72 @@ export const Dropdown = forwardRef<React.ComponentRef<typeof DropdownMenu.Conten
   }
 )
 Dropdown.displayName = 'Dropdown'
+
+type DropdownLabelProps = ComponentPropsWithoutRef<typeof DropdownMenu.Label>
+
+export const DropdownLabel = forwardRef<
+  React.ComponentRef<typeof DropdownMenu.Label>,
+  DropdownLabelProps
+>(({ children, className, ...rest }, ref) => {
+  const classNames = {
+    label: clsx(s.label, className),
+  }
+
+  return (
+    <DropdownMenu.Label className={classNames.label} ref={ref} {...rest}>
+      {children}
+    </DropdownMenu.Label>
+  )
+})
+
+DropdownLabel.displayName = 'DropdownLabel'
+
+type DropdownSeparatorProps = ComponentPropsWithoutRef<typeof DropdownMenu.Separator>
+
+export const DropdownSeparator = forwardRef<
+  React.ComponentRef<typeof DropdownMenu.Separator>,
+  DropdownSeparatorProps
+>(({ className, ...rest }, ref) => {
+  const classNames = {
+    separator: clsx(s.separator, className),
+  }
+
+  return <DropdownMenu.Separator ref={ref} className={classNames.separator} {...rest} />
+})
+
+DropdownSeparator.displayName = 'DropdownSeparator'
+
+type DropdownArrowProps = Omit<ComponentPropsWithoutRef<'div'>, 'onChange'>
+
+export const DropdownArrow = forwardRef<React.ComponentRef<'div'>, DropdownArrowProps>(
+  ({ children, className, ...rest }, ref) => {
+    const classNames = {
+      arrow: clsx(s.arrow, className),
+    }
+    return (
+      <DropdownMenu.Arrow asChild>
+        <div ref={ref} className={classNames.arrow} {...rest} />
+      </DropdownMenu.Arrow>
+    )
+  }
+)
+
+DropdownArrow.displayName = 'DropdownArrow'
+
+type DropdownItemProps = ComponentPropsWithoutRef<typeof DropdownMenu.Item>
+
+export const DropdownItem = forwardRef<
+  React.ComponentRef<typeof DropdownMenu.Item>,
+  DropdownItemProps
+>(({ children, className, ...rest }, ref) => {
+  const classNames = {
+    item: clsx(s.item, className),
+  }
+  return (
+    <DropdownMenu.Item ref={ref} className={classNames.item} {...rest}>
+      {children}
+    </DropdownMenu.Item>
+  )
+})
+
+DropdownItem.displayName = 'DropdownItem'
