5beeca0b3f270ec8f975a2c53b8385624568bc26
Evgeniy Ivanov <p8635241@gmail.com>
2025-04-04 23:00:13 +0500

    IN-33 feat: add email sent modal for review

--- entities/EmailSentModal/EmailSentModal.tsx
+++ entities/EmailSentModal/EmailSentModal.tsx
@@ -1,16 +1,28 @@
 import { Button, Modal, Typography } from '@/shared/components'
 import s from './EmailSentModal.module.scss'
+import { useState } from 'react'
 
 type Props = {
   email: string
 }
 
 export const EmailSentModal = ({ email }: Props) => {
+  const [openModal, setOpenModal] = useState(true)
+
+  const modalOpenHandler = () => setOpenModal(false)
+
   return (
-    <Modal open className="modal-header-override" title="Email sent">
+    <Modal
+      className="modal-header-override"
+      open={openModal}
+      title="Email sent"
+      onOpenChange={modalOpenHandler}
+    >
       <div className={s.container}>
         <Typography variant="regular_16">We have sent a link to confirm your {email}</Typography>
-        <Button className={s.button}>OK</Button>
+        <Button className={s.button} onClick={modalOpenHandler}>
+          OK
+        </Button>
       </div>
     </Modal>
   )
