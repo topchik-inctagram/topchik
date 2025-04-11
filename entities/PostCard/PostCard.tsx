c098d69ac561515dab1562b6eb3a1871954d13a8
Evgeniy Ivanov <p8635241@gmail.com>
2025-04-12 03:03:06 +0500

    IN-58 fix: fix postcard styles

--- entities/PostCard/PostCard.tsx
+++ entities/PostCard/PostCard.tsx
@@ -2,9 +2,11 @@ import Image, { type StaticImageData } from 'next/image'
 import s from './PostCard.module.scss'
 import { Typography } from '@/shared/components'
 import { useState } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation } from 'swiper/modules'
-
+import { ArrowIosBackOutline, ArrowIosForwardOutline } from '@/public/icons'
+import useEmblaCarousel from 'embla-carousel-react'
+import { usePrevNextButtons } from '@/entities/PostCard/usePrevNextButton'
+import { DotButton, useDotButton } from '@/entities/PostCard/useDotButton'
+import { clsx } from 'clsx'
 type Props = {
   postImage: StaticImageData | StaticImageData[]
   authorImage: StaticImageData
@@ -14,6 +16,11 @@ type Props = {
 }
 
 export const PostCard = ({ postText, postCreated, postImage, postTitle, authorImage }: Props) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel()
+  const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } =
+    usePrevNextButtons(emblaApi)
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(emblaApi)
+
   const [showMoreButton, setShowMoreButton] = useState(false)
   const postTextCut = postText.length > 82 ? postText.slice(0, 83) + '...' : postText
   const [postTextToDisplay, setPostTextToDisplay] = useState(postTextCut)
@@ -31,16 +38,47 @@ export const PostCard = ({ postText, postCreated, postImage, postTitle, authorIm
   return (
     <section className={s.container}>
       {Array.isArray(postImage) ? (
-        <Swiper navigation modules={[Navigation]}>
-          {postImage.map((t, i) => (
-            <SwiperSlide key={i}>
-              <Image alt="post image" className={s.postPhoto} src={t} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <div ref={emblaRef} className={s.embla__viewport}>
+          <div className={s.embla__container}>
+            {postImage.map((t, i) => (
+              <Image key={i} alt="post image" className={s.postPhoto} src={t} />
+            ))}
+          </div>
+        </div>
       ) : (
         <Image alt="post image" className={s.postPhoto} src={postImage} />
       )}
+
+      <div className={s.embla__controls}>
+        <div className={s.embla__buttons}>
+          <button
+            className={clsx(s.embla__buttons, s.embla__button)}
+            disabled={prevBtnDisabled}
+            type="button"
+            onClick={onPrevButtonClick}
+          >
+            <ArrowIosBackOutline />
+          </button>
+          <button
+            className={clsx(s.embla__buttons, s.embla__button)}
+            disabled={nextBtnDisabled}
+            type="button"
+            onClick={onNextButtonClick}
+          >
+            <ArrowIosForwardOutline />
+          </button>
+        </div>
+      </div>
+      <div className={s.embla__dots}>
+        {Array.isArray(postImage) &&
+          scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              className={clsx(s.embla__dot, index === selectedIndex && s.embla__dot__selected)}
+              onClick={() => onDotButtonClick(index)}
+            />
+          ))}
+      </div>
 
       <div className={s.postContainer}>
         <div className={s.imageContainer}>
@@ -53,7 +91,7 @@ export const PostCard = ({ postText, postCreated, postImage, postTitle, authorIm
         <Typography className={s.postText} variant="regular_14">
           {postTextToDisplay}
           {postTextTooLong && (
-            <button className={s.showMoreButton} onClick={showMoreButtonHandler}>
+            <button className={s.showMoreButton} type="button" onClick={showMoreButtonHandler}>
               <Typography as="span" variant="regular_14">
                 {showMoreButton ? 'Hide' : 'Show more'}
               </Typography>
