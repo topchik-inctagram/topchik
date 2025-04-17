e549b12cb493aced2d61188c86cc9b4965084369
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2025-04-17 13:48:20 +0300

    IN-71 feat: extension props PageContainer

--- shared/components/PageContainer/PageContainer.tsx
+++ shared/components/PageContainer/PageContainer.tsx
@@ -5,11 +5,13 @@ import s from './PageContainer.module.scss'
 
 type Props = ComponentPropsWithRef<'main'> & {
   mt?: CSSProperties['marginTop']
+  direction?: 'row' | 'column'
 }
 
-export function PageContainer({ className, mt = '24px', style, ref, ...rest }: Props) {
+export function PageContainer({ className, mt = '24px', direction, style, ref, ...rest }: Props) {
   const classNames = {
-    page: clsx(className, s.container),
+    page: clsx(className, s.container, direction === 'row' && s.row,
+      direction === 'column' && s.column),
   }
   const styles: CSSProperties = { marginTop: mt, ...style }
 
