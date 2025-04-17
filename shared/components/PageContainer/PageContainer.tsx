a2904edde4f6d28f04522ba53bd1c8d4e60aca04
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2025-04-17 16:32:07 +0300

    IN-71 fix: update PageContainer

--- shared/components/PageContainer/PageContainer.tsx
+++ shared/components/PageContainer/PageContainer.tsx
@@ -5,15 +5,28 @@ import s from './PageContainer.module.scss'
 
 type Props = ComponentPropsWithRef<'main'> & {
   mt?: CSSProperties['marginTop']
+  pr?: CSSProperties['paddingRight']
+  pl?: CSSProperties['paddingLeft']
   direction?: 'row' | 'column'
 }
 
-export function PageContainer({ className, mt = '24px', direction, style, ref, ...rest }: Props) {
+export function PageContainer({
+  className,
+  mt = '24px',
+  pr,
+  pl,
+  direction,
+  style,
+  ref,
+  ...rest }: Props) {
   const classNames = {
     page: clsx(className, s.container, direction === 'row' && s.row,
       direction === 'column' && s.column),
   }
-  const styles: CSSProperties = { marginTop: mt, ...style }
+  const styles: CSSProperties = { marginTop: mt,
+    paddingRight: pr,
+    paddingLeft: pl,
+    ...style }
 
   return <main ref={ref} className={classNames.page} style={styles} {...rest} />
 }
