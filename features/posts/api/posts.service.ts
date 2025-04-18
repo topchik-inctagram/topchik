365265165789d2764530de1c5a189dcc9441b019
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2025-04-18 21:44:27 +0300

    IN-71 feat: create getPosts endpoint

---
+++ features/posts/api/posts.service.ts
@@ -0,0 +1,18 @@
+import {baseApi} from '@/shared/store';
+import { type PostsResponse} from '@/features/posts/api/posts.types';
+
+export const PostsService = baseApi.injectEndpoints({
+  endpoints: builder => {
+    return {
+      getPosts: builder.query<PostsResponse, number | void>({
+        providesTags: [''],
+        query: (cursor) => ({
+          params: cursor ? cursor : {},
+          url: '/api/v1/posts',
+        }),
+      }),
+    }
+  },
+})
+
+export const { useGetPostsQuery } = PostsService
